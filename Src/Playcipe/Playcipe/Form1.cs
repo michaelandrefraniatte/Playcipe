9ab70ca45c9a14719022019e85536b17690b77c7
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2023-10-28 15:24:08 +0200

    Fix a bug

--- Src/Playcipe/Playcipe/Form1.cs
+++ Src/Playcipe/Playcipe/Form1.cs
@@ -323,176 +323,134 @@ namespace Playcipe
             {
                 string stringinject = @"
                     if (window.location.href.indexOf('youtube') > -1 | window.location.href.indexOf('youtu.be') > -1) {
-                        
-                        const bridge = chrome.webview.hostObjects.bridge;
-
-                        (function () {
-                            getScript('https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js', function () {
-                                $(document).ready(function () {
-                                    removeAds();
-                                    var playButton = getPlayButton();
-                                    if (playButton) {
-                                        playButton.click();
-                                    }
-                                    var skipButton = getSkipButton();
-                                    if (skipButton) {
-                                        skipButton.click();
-                                    }
-                                    var closeButton = getCloseButton();
-                                    if (closeButton) {
-                                        closeButton.click();
-                                    }
-                                });
-                            });
-                            function getScript(url, success) {
-                                var script = document.createElement('script');
-                                script.src = url;
-                                var head = document.getElementsByTagName('head')[0],
-                                    done = false;
-                                script.onload = script.onreadystatechange = function () {
-                                    if (!done && (!this.readyState
-                                        || this.readyState == 'loaded'
-                                        || this.readyState == 'complete')) {
-                                        done = true;
-                                        success();
-                                        script.onload = script.onreadystatechange = null;
-                                        head.removeChild(script);
-                                    }
-                                };
-                                head.appendChild(script);
+                        try {
+                            var playButton = document.querySelector('.ytp-large-play-button:visible');
+                            if (playButton) {
+                                playButton.click();
                             }
-                        })();
-
-                        function removeAds() {
-                            try {
-                                var mute = document.querySelectorAll('.ad-showing');
-                                if (mute.length > 0) {
-                                    bridge.CutSound('1');
-                                }
-                                else {
-                                    bridge.CutSound('0');
-                                }
-                            }
-                            catch { }
-                            try {
-                                document.cookie = 'VISITOR_INFO1_LIVE = oKckVSqvaGw; path =/; domain =.youtube.com';
-                                var cookies = document.cookie.split('; ');
-                                for (var i = 0; i < cookies.length; i++)
-                                {
-                                    var cookie = cookies[i];
-                                    var eqPos = cookie.indexOf('=');
-                                    var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-                                    document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
-                                }
-                            }
-                            catch { }
-                            try {
-                                var els = document.getElementsByClassName('video-ads ytp-ad-module');
-                                for (var i=0;i<els.length; i++) {
-                                    els[i].click();
-                                }
-                            }
-                            catch { }
-                            try {
-                                var el = document.getElementsByClassName('ytp-ad-skip-button');
-                                for (var i=0;i<el.length; i++) {
-                                    el[i].click();
-                                }
-                            }
-                            catch { }
-                            try {
-                                var element = document.getElementsByClassName('ytp-ad-overlay-close-button');
-                                for (var i=0;i<element.length; i++) {
-                                    element[i].click();
-                                }
-                            }
-                            catch { }
-                            try {
-                                var scripts = document.getElementsByTagName('script');
-                                for (let i = 0; i < scripts.length; i++)
-                                {
-                                    var content = scripts[i].innerHTML;
-                                    if (content.indexOf('ytp-ad') > -1) {
-                                        scripts[i].innerHTML = '';
-                                    }
-                                    var src = scripts[i].getAttribute('src');
-                                    if (src.indexOf('ytp-ad') > -1) {
-                                        scripts[i].setAttribute('src', '');
-                                    }
-                                }
-                            }
-                            catch { }
-                            try {
-                                var iframes = document.getElementsByTagName('iframe');
-                                for (let i = 0; i < iframes.length; i++)
-                                {
-                                    var content = iframes[i].innerHTML;
-                                    if (content.indexOf('ytp-ad') > -1) {
-                                        iframes[i].innerHTML = '';
-                                    }
-                                    var src = iframes[i].getAttribute('src');
-                                    if (src.indexOf('ytp-ad') > -1) {
-                                        iframes[i].setAttribute('src', '');
-                                    }
-                                }
-                            }
-                            catch { }
-                            try {
-                                var allelements = document.querySelectorAll('*');
-                                for (var i = 0; i < allelements.length; i++) {
-                                    var classname = allelements[i].className;
-                                    if (classname.indexOf('ytp-ad') > -1 | classname.indexOf('-ad-') > -1 | classname.indexOf('ad-') > -1 | classname.indexOf('ads-') > -1 | classname.indexOf('ad-showing') > -1 | classname.indexOf('ad-container') > -1 | classname.indexOf('ytp-ad-overlay-open') > -1 | classname.indexOf('video-ads') > -1)  {
-                                        allelements[i].innerHTML = '';
-                                    }
-                                }
-                            }
-                            catch { }
-                            try {
-                                var players = document.getElementById('movie_player');
-                                for (let i = 0; i < players.length; i++) {
-                                    players.classList.remove('ad-interrupting');
-                                    players.classList.remove('playing-mode');
-                                    players.classList.remove('ytp-autohide');
-                                    players.classList.add('ytp-hide-info-bar');
-                                    players.classList.add('playing-mode');
-                                    players.classList.add('ytp-autohide');
-                                }
-                            }
-                            catch { }
-                            try {
-                                var fabelements = document.querySelectorAll('yt-reaction-control-panel-button-view-model');
-                                for (var i = 0; i < fabelements.length; i++) {
-                                     fabelements[i].innerHTML = '';
-                                }
-                            }
-                            catch { }
-                            try {
-                                var fabelement = document.querySelector('#fab-container');
-                                fabelement.innerHTML = '';
-                            }
-                            catch { }
                         }
-
-                        function getPlayButton() {
-                            return $('.ytp-large-play-button:visible');
-                        }
-
-                        function getSkipButton() {
-                            return $('.ytp-ad-skip-button');
-                        }
-
-                        function getCloseButton() {
-                            return $('.ytp-ad-overlay-close-button');
-                        }
-                        
-                        var stringinject = `
-                        <style>
-                            .ad-showing, .ad-container, .ytp-ad-overlay-open, .video-ads, .ytp-ad-overlay-image, .ytp-ad-overlay-container, .ytd-carousel-ad-renderer, ytd-ad-slot-renderer, ytd-action-companion-ad-renderer, ytd-engagement-panel-section-list-renderer, ytd-player-legacy-desktop-watch-ads-renderer, #reaction-control-panel, #emoji-fountain, #fab-container, yt-reaction-control-panel-button-view-model {
-                                display: none !important;
+                        catch { }
+                        try {
+                            var skipButton = document.querySelector('.ytp-ad-skip-button');
+                            if (skipButton) {
+                                skipButton.click();
                             }
-                        </style>`;
-                        document.getElementsByTagName('head')[0].innerHTML += stringinject;
-                        
+                        }
+                        catch { }
+                        try {
+                            var closeButton = document.querySelector('.ytp-ad-overlay-close-button');
+                            if (closeButton) {
+                                closeButton.click();
+                            }
+                        }
+                        catch { }
+                        try {
+                            document.cookie = 'VISITOR_INFO1_LIVE = oKckVSqvaGw; path =/; domain =.youtube.com';
+                            var cookies = document.cookie.split('; ');
+                            for (var i = 0; i < cookies.length; i++)
+                            {
+                                var cookie = cookies[i];
+                                var eqPos = cookie.indexOf('=');
+                                var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+                                document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
+                            }
+                        }
+                        catch { }
+                        try {
+                            var els = document.getElementsByClassName('video-ads ytp-ad-module');
+                            for (var i=0;i<els.length; i++) {
+                                els[i].click();
+                            }
+                        }
+                        catch { }
+                        try {
+                            var el = document.getElementsByClassName('ytp-ad-skip-button');
+                            for (var i=0;i<el.length; i++) {
+                                el[i].click();
+                            }
+                        }
+                        catch { }
+                        try {
+                            var element = document.getElementsByClassName('ytp-ad-overlay-close-button');
+                            for (var i=0;i<element.length; i++) {
+                                element[i].click();
+                            }
+                        }
+                        catch { }
+                        try {
+                            var scripts = document.getElementsByTagName('script');
+                            for (let i = 0; i < scripts.length; i++)
+                            {
+                                var content = scripts[i].innerHTML;
+                                if (content.indexOf('ytp-ad') > -1) {
+                                    scripts[i].innerHTML = '';
+                                }
+                                var src = scripts[i].getAttribute('src');
+                                if (src.indexOf('ytp-ad') > -1) {
+                                    scripts[i].setAttribute('src', '');
+                                }
+                            }
+                        }
+                        catch { }
+                        try {
+                            var iframes = document.getElementsByTagName('iframe');
+                            for (let i = 0; i < iframes.length; i++)
+                            {
+                                var content = iframes[i].innerHTML;
+                                if (content.indexOf('ytp-ad') > -1) {
+                                    iframes[i].innerHTML = '';
+                                }
+                                var src = iframes[i].getAttribute('src');
+                                if (src.indexOf('ytp-ad') > -1) {
+                                    iframes[i].setAttribute('src', '');
+                                }
+                            }
+                        }
+                        catch { }
+                        try {
+                            var allelements = document.querySelectorAll('*');
+                            for (var i = 0; i < allelements.length; i++) {
+                                var classname = allelements[i].className;
+                                if (classname.indexOf('ytp-ad') > -1 | classname.indexOf('-ad-') > -1 | classname.indexOf('ad-') > -1 | classname.indexOf('ads-') > -1 | classname.indexOf('ad-showing') > -1 | classname.indexOf('ad-container') > -1 | classname.indexOf('ytp-ad-overlay-open') > -1 | classname.indexOf('video-ads') > -1)  {
+                                    allelements[i].innerHTML = '';
+                                }
+                            }
+                        }
+                        catch { }
+                        try {
+                            var players = document.getElementById('movie_player');
+                            for (let i = 0; i < players.length; i++) {
+                                players.classList.remove('ad-interrupting');
+                                players.classList.remove('playing-mode');
+                                players.classList.remove('ytp-autohide');
+                                players.classList.add('ytp-hide-info-bar');
+                                players.classList.add('playing-mode');
+                                players.classList.add('ytp-autohide');
+                            }
+                        }
+                        catch { }
+                        try {
+                            var fabelements = document.querySelectorAll('yt-reaction-control-panel-button-view-model');
+                            for (var i = 0; i < fabelements.length; i++) {
+                                    fabelements[i].innerHTML = '';
+                            }
+                        }
+                        catch { }
+                        try {
+                            var fabelement = document.querySelector('#fab-container');
+                            fabelement.innerHTML = '';
+                        }
+                        catch { }
+                        try {
+                            var stringinject = `
+                            <style>
+                                .ad-showing, .ad-container, .ytp-ad-overlay-open, .video-ads, .ytp-ad-overlay-image, .ytp-ad-overlay-container, .ytd-carousel-ad-renderer, ytd-ad-slot-renderer, ytd-action-companion-ad-renderer, ytd-engagement-panel-section-list-renderer, ytd-player-legacy-desktop-watch-ads-renderer, #reaction-control-panel, #emoji-fountain, #fab-container, yt-reaction-control-panel-button-view-model {
+                                    display: none !important;
+                                }
+                            </style>`;
+                            document.getElementsByTagName('head')[0].innerHTML += stringinject;
+                        }
+                        catch { }
                     }
                     ";
                 await execScriptHelper(stringinject);
@@ -501,11 +459,30 @@ namespace Playcipe
         }
         private async void timer2_Tick(object sender, EventArgs e)
         {
+            string stringinject = "";
+            try
+            {
+                stringinject = @"
+                    try {
+                        const bridge = chrome.webview.hostObjects.bridge;
+                        var mute = document.querySelectorAll('.ad-showing');
+                        if (mute.length > 0) {
+                            bridge.CutSound('1');
+                        }
+                        else {
+                            bridge.CutSound('0');
+                        }
+                    }
+                    catch { }
+                    ";
+                await execScriptHelper(stringinject);
+            }
+            catch { }
             KeyboardHookProcessButtons();
             valchanged(0, Key_PAGE_DOWN);
             if (wu[0] == 1)
             {
-                string stringinject = @"
+                stringinject = @"
                     window.scrollBy(0, window.innerHeight);
                 ".Replace("\r\n", " ");
                 await execScriptHelper(stringinject);
@@ -513,7 +490,7 @@ namespace Playcipe
             valchanged(1, Key_PAGE_UP);
             if (wu[1] == 1)
             {
-                string stringinject = @"
+                stringinject = @"
                     window.scrollBy(0, -window.innerHeight);
                 ".Replace("\r\n", " ");
                 await execScriptHelper(stringinject);
