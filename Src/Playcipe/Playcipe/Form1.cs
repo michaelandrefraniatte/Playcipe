4156f5407097c78adeb0ebf573b34de95e7f6e7e
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2023-11-25 18:21:17 +0100

    Update configs

--- Src/Playcipe/Playcipe/Form1.cs
+++ Src/Playcipe/Playcipe/Form1.cs
@@ -337,6 +337,13 @@ namespace Playcipe
                         }
                         catch { }
                         try {
+                            var skipButton = document.querySelector('.ytp-ad-skip-button-modern');
+                            if (skipButton) {
+                                skipButton.click();
+                            }
+                        }
+                        catch { }
+                        try {
                             var closeButton = document.querySelector('.ytp-ad-overlay-close-button');
                             if (closeButton) {
                                 closeButton.click();
