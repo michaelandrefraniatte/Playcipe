e72d60706dc47268802e49be383eae233b4b6371
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-06-08 07:57:20 +0200

    Fix bug

--- Src/blockads-extension-v3/inject.js
+++ Src/blockads-extension-v3/inject.js
@@ -7,39 +7,33 @@ function blockAds(videoid) {
     timerid = setInterval(() => {
         var adshowing = document.querySelector('.ad-showing');
         if (adshowing) {
-            try {
-                var skipmodernbutton = document.querySelector('.ytp-ad-skip-button-modern');
-                if (skipmodernbutton) {
-                    skipmodernbutton.click();
-                }
-                var skipbutton = document.querySelector('.ytp-ad-skip-button');
-                if (skipbutton) {
-                    skipbutton.click();
-                }
-                var player = document.getElementById('player');
-                if (player) {
-                    if (player.style.backgroundImage != 'url("https://i.ytimg.com/vi/' + videoid + '/hq720.jpg")') {
-                        player.style.backgroundImage = 'url("https://i.ytimg.com/vi/' + videoid + '/hq720.jpg")';
-                        player.style.backgroundSize = 'cover';
-                        player.style.backgroundRepeat = 'no-repeat';
-                        player.style.backgroundPosition = 'center';
-                    }
+            var modernskipbutton = $('.ytp-ad-skip-button-modern');
+            if (modernskipbutton.length > 0) {
+                modernskipbutton.click();
+            }
+            var skipbutton = $('.ytp-ad-skip-button');
+            if (skipbutton.length > 0) {
+                skipbutton.click();
+            }
+            var player = document.getElementById('player');
+            if (player) {
+                if (player.style.backgroundImage != 'url("https://i.ytimg.com/vi/' + videoid + '/hq720.jpg")') {
+                    player.style.backgroundImage = 'url("https://i.ytimg.com/vi/' + videoid + '/hq720.jpg")';
+                    player.style.backgroundSize = 'cover';
+                    player.style.backgroundRepeat = 'no-repeat';
+                    player.style.backgroundPosition = 'center';
                 }
             }
-            catch { }
         }
         else {
-            try {
-                var player = document.getElementById('player');
-                if (player) {
-                    player.style.backgroundImage = 'none';
-                }
-                var video = document.querySelector('.html5-video-player');
-                if (video) {
-                    video.style.display = 'block';
-                }
+            var player = document.getElementById('player');
+            if (player) {
+                player.style.backgroundImage = 'none';
             }
-            catch { }
+            var video = document.querySelector('.html5-video-player');
+            if (video) {
+                video.style.display = 'block';
+            }
         }
     }, 1000);
     if (timeridtemp != timerid) {
