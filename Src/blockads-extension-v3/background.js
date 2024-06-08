6ab5235388c3e7fb0edd4d863025992f3b1bcc8d
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-06-08 04:29:07 +0200

    Fix issue

--- Src/blockads-extension-v3/background.js
+++ Src/blockads-extension-v3/background.js
@@ -1,30 +1,75 @@
 
-// adds a listener to tab change
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    // check for a URL in the changeInfo parameter
-    if (changeInfo.url.startsWith('https://www.youtube.com/')) {
-        // calls the inject function
-        injectScript(tabId);
-    }
-    // check for a URL in the tab parameter
-    if (tab.url.startsWith('https://www.youtube.com/')) {
-        // calls the inject function
-        injectScript(tabId);
+chrome.webNavigation.onCompleted.addListener(function (details) {
+    if (details.url.includes('watch?v=')) {
+        var link = details.url;
+        if (link.includes('&')) {
+            link = link.split('&')[0];
+        }
+        var videoid = link.split('watch?v=')[1];
+        chrome.scripting.executeScript(
+            {
+                target: { tabId: details.tabId },
+                func: blockAds,
+                args: [videoid]
+            }
+        )
     }
 });
 
-// function that injects code to a specific tab
-function injectScript(tabId) {
-    chrome.scripting.executeScript(
-        {
-            target: { tabId: tabId },
-            files: ['inject.js'],
-        },
-        () => {
-            chrome.scripting.executeScript({
-                target: { tabId },
-                args: [tabId, changeInfo, tab],
-                func: (...args) => startBlockAds(...args),
-            });
-        })
-};
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (tab.url.includes('watch?v=')) {
+        var link = tab.url;
+        if (link.includes('&')) {
+            link = link.split('&')[0];
+        }
+        var videoid = link.split('watch?v=')[1];
+        chrome.scripting.executeScript(
+            {
+                target: { tabId: tabId },
+                func: blockAds,
+                args: [videoid]
+            }
+        )
+    }
+});
+
+function blockAds(videoid) {
+    setInterval(() => {
+        var adshowing = document.querySelector('.ad-showing');
+        if (adshowing) {
+            try {
+                var player = document.getElementById('player');
+                if (player) {
+                    if (player.style.backgroundImage != 'url(https://i.ytimg.com/vi/' + videoid + '/hq720.jpg)') {
+                        player.style.backgroundImage = 'url(https://i.ytimg.com/vi/' + videoid + '/hq720.jpg)';
+                        player.style.backgroundSize = 'cover';
+                        player.style.backgroundRepeat = 'no-repeat';
+                        player.style.backgroundPosition = 'center';
+                    }
+                }
+                var skipmodernbutton = document.querySelector('.ytp-ad-skip-button-modern');
+                if (skipmodernbutton) {
+                    skipmodernbutton.click();
+                }
+                var skipbutton = document.querySelector('.ytp-ad-skip-button');
+                if (skipbutton) {
+                    skipbutton.click();
+                }
+            }
+            catch { }
+        }
+        else {
+            try {
+                var player = document.getElementById('player');
+                if (player) {
+                    player.style.backgroundImage = 'none';
+                }
+                var video = document.querySelector('.html5-video-player');
+                if (video) {
+                    video.style.display = 'block';
+                }
+            }
+            catch { }
+        }
+    }, 1000);
+}
