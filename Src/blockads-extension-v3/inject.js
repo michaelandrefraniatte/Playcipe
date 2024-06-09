108632c0bb0cf3647584ad64c969a8d7ee8e69d1
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-06-09 08:20:11 +0200

    Update inject.js

--- Src/blockads-extension-v3/inject.js
+++ Src/blockads-extension-v3/inject.js
@@ -1,11 +1,18 @@
 
+let videoidtemp1;
+let videoidtemp2;
+
 function blockAds(videoid) {
-    var sj = document.createElement('script');
-    sj.src = chrome.runtime.getURL('jquery-3.7.1.min.js');
-    sj.onload = function() { this.remove(); };
-    (document.head || document.documentElement).appendChild(sj);
-    var s = document.createElement('script');
-    s.src = chrome.runtime.getURL('script.js');
-    s.onload = function() { this.remove(); };
-    (document.head || document.documentElement).appendChild(s);
+    videoidtemp1 = videoidtemp2;
+    videoidtemp2 = videoid;
+    if (videoidtemp1 != videoidtemp2) {
+        var sj = document.createElement('script');
+        sj.src = chrome.runtime.getURL('jquery-3.7.1.min.js');
+        sj.onload = function() { this.remove(); };
+        (document.head || document.documentElement).appendChild(sj);
+        var s = document.createElement('script');
+        s.src = chrome.runtime.getURL('script.js');
+        s.onload = function() { this.remove(); };
+        (document.head || document.documentElement).appendChild(s);
+    }
 }
